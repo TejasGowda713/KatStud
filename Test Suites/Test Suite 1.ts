2a4953ae39d0eef13e884341ec0809d63ab49d17
TejasGowda713 <tejasgowda713@gmail.com>
2021-04-22 20:27:33 +0530

    first commit

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fed5a56c-0489-40ba-b582-72ec3243ce0a</testSuiteGuid>
+   <testCaseLink>
+      <guid>53205011-b26e-490a-b573-4f62a176eb36</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manual WebTest/Succesful Login Attempt</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
